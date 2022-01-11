0ae45b954b5b45d0f149412c043e9aa5053edf5c
CameraTim <93443049+CameraTim@users.noreply.github.com>
2022-01-10 23:29:32 -0500

    Added first JavaScript file

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+var A = "This is the first ", B = "and this is the second. ", C = "This third one just does whatever it wants."; //Variables are being assigned to strings
+var A = A.fontcolor("Blue"), B = B.fontcolor("Green"), C = C.fontcolor("Red");//Variables are being assigned a color
+document.write(A, B, C); //The webpage is writing out the variables
+window.alert(3*3) //This is an expression showing up in an alert box, since alerts are expressive, it just makes sense.
